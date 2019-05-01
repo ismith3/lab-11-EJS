1d2582578bea952bc1fc24bb8ee21fffc82e42c0
Ian Smith <ianmsmith17@gmail.com>
2019-05-01 11:26:10 -0500

    styled results page some more

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.static('./public'));
 app.listen(PORT, () => console.log(`Never fear... port ${PORT} is here!!`));
 
 /*render index page*/
-  app.get('/', (request, response) => {
+app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
 
 /* Book fetching*/
 app.post('/results', (request, response) => {
-  
+
   console.log(request.body);
   let input;
   if(request.body.searchOption === 'title'){
@@ -51,7 +51,7 @@ function fetchBooks (input, response){
 
 function Book(book){
   this.title = book.volumeInfo.title || 'Not Found';
-  this.authors = book.volumeInfo.authors || 'Not Found';
+  this.authors = book.volumeInfo.authors.join() || 'Not Found';
   this.description = book.volumeInfo.description || 'Not Found';
   this.image = book.volumeInfo.imageLinks.thumbnail.replace('http', 'https') || 'Not Found';
 }
